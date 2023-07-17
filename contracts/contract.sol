3eec22ae33450bd9a65d38265f7f5ab07e82e2d6
Yogendra Sankhla <yogendrasankhla@Yogendras-MacBook-Pro.local>
2023-07-18 00:30:21 +0530

    aa simple

--- contracts/contract.sol
+++ contracts/contract.sol
@@ -4,9 +4,41 @@ pragma solidity ^0.8.9;
 
 contract Account{
 
-
+   string public name;
+   address public owner;
+   address public backupAccount;
+   address public newOwner;
     
 
+   constructor(address _backupAccount, string memory _name) {
+         owner = msg.sender;
+         backupAccount = _backupAccount;
+         name = _name;
+
+   }
+   
+   function withdraw(uint amount) public {
+       require(msg.sender == owner, "You are not the owner");
+       payable(msg.sender).transfer(amount);
+   }
+
+   function call(address _contractAddress, bytes memory _data) public {
+       require(msg.sender == owner, "You are not the owner");
+       (bool success,) = _contractAddress.call(_data);
+       require(success, "Call failed");
+   }
+
+  function claimOwnership(address _newbackupAccount) public {
+       require(backupAccount == msg.sender, "You are not the backup account");
+         backupAccount = _newbackupAccount;
+         owner = msg.sender;
+   }
+    
+   function changebackupAccount(address _newbackupAccount) public {
+       require(owner == msg.sender, "You are not the owner");
+         backupAccount = _newbackupAccount;
+   }
 
 
 }
+
